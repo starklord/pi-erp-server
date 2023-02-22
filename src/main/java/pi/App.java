fdf9422eacdbe55ef69be3beb24e67da0d77c588
starklord <evanlance@gmail.com>
2023-02-22 16:48:08 -0500

    fds

--- src/main/java/pi/App.java
+++ src/main/java/pi/App.java
@@ -38,12 +38,12 @@ public class App {
         Server.DB_USR = "postgres";
         Server.DB_PWD = "evadb7007";
         Server.DB_DRIVER = "org.postgresql.Driver";
-        Server.IP_SERVER = Util.EVAS[4];  
-        // Server.IP_SERVER = "localhost";  
+        Server.IP_SERVER = Util.EVAS[4];
+        // Server.IP_SERVER = "localhost";
         Server.DB_PORT = 7077;
-        CConexion.strDriver = Server.DB_DRIVER;   
+        CConexion.strDriver = Server.DB_DRIVER;
         CConexion.strPwd = Server.DB_PWD; 
-        CConexion.strUsr = Server.DB_USR;   
+        CConexion.strUsr = Server.DB_USR;     
         CConexion.IP_SERVER = Server.IP_SERVER;
         CConexion.port = Server.DB_PORT;
         System.out.println("datos de la base de datos cargados exitosamente");
